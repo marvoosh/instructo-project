ccd2f1553253211f8cb8f81a5edce6a9a055d8cc
mn <marvoosh@gmail.com>
2021-11-17 13:03:41 +0000

    seed file changed and added button on confirmation - continue browsing

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -2,7 +2,7 @@ class InstructorsController < ApplicationController
   # IMAGES = ["hUHzaiAHuUc", "EzH46XCDQRY", "E6ExxeQNiN4", "iE71-TMrrkE", "9fByQORuvqM", "IrRbSND5EUc", "tXJhAFVOHVk", "rM6tUdA8UyE"]
   def index
     @instructors = Instructor.all
-    @images = ["hUHzaiAHuUc", "EzH46XCDQRY", "E6ExxeQNiN4", "iE71-TMrrkE", "9fByQORuvqM", "IrRbSND5EUc", "tXJhAFVOHVk", "rM6tUdA8UyE"]
+    @images = ["hUHzaiAHuUc", "EzH46XCDQRY", "E6ExxeQNiN4", "iE71-TMrrkE", "9fByQORuvqM", "IrRbSND5EUc", "tXJhAFVOHVk", "rM6tUdA8UyE", "v9FQR4tbIq8", "SVfXlChg9HI", "UYG1U5wj3Tk", "Z9AU36chmQI"]
   end
 
   def new
