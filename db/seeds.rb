8b911a9ecd3785275d0b0ce36db2d2df9eee4b9c
mn <marvoosh@gmail.com>
2021-11-19 12:11:43 +0000

    added new pics to profiles

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,10 @@
 
 # User.destroy_all
 
-user1 = User.create(first_name: 'Buffy', last_name: 'Summers', email: "buffy@test.com", password: '123123',instructor: true, profile_url: 'https://hips.hearstapps.com/digitalspyuk.cdnds.net/15/50/1449943322-gettyimages-51103216-buffy.jpg')
-user2 = User.create(first_name: 'Willow', last_name: 'Rosenberg', email: "willow@test.com", password: '123123',instructor: true, profile_url: 'https://junkee.com/wp-content/uploads/2017/03/willow-2.jpg')
-user3 = User.create(first_name: 'Xander', last_name: 'Harris', email: "xander@test.com", password: '123123', profile_url: 'https://m.media-amazon.com/images/M/MV5BMTM3NTA3MzM3N15BMl5BanBnXkFtZTYwMTY4NjEz._V1_UY1200_CR172,0,630,1200_AL_.jpg')
-user4 = User.create(first_name: 'Angel', last_name: ' ', email: "angel@test.com", password: '123123', profile_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_u-knlE4hna_4fnfTkgMKPLLXpI6pm6dzlQ&usqp=CAU')
+user1 = User.create(first_name: 'Buffy', last_name: 'Summers', email: "buffy@test.com", password: '123123',instructor: true, profile_url: 'https://upload.wikimedia.org/wikipedia/en/a/a2/S514_Buffy.png')
+user2 = User.create(first_name: 'Willow', last_name: 'Rosenberg', email: "willow@test.com", password: '123123',instructor: true, profile_url: 'https://openpsychometrics.org/tests/characters/test-resources/pics/BVS/3.jpg')
+user3 = User.create(first_name: 'Xander', last_name: 'Harris', email: "xander@test.com", password: '123123', profile_url: 'https://openpsychometrics.org/tests/characters/test-resources/pics/BVS/2.jpg')
+user4 = User.create(first_name: 'Angel', last_name: ' ', email: "angel@test.com", password: '123123', profile_url: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/top-1553084314.jpg?resize=480:*')
 user5 = User.create(first_name: 'Marva', last_name: 'Noah', email: "marva@test.com", password: '123123', profile_url: 'https://ca.slack-edge.com/T02NE0241-U02GFM63B8B-3067df17e3fa-512')
 
 Instructor.destroy_all
@@ -23,7 +23,7 @@ Instructor.create!(
   user_id: user1.id,
   price: 109,
   bio: 'If you got vampires, I can show you how to slay them!',
-  profile_url: 'https://hips.hearstapps.com/digitalspyuk.cdnds.net/15/50/1449943322-gettyimages-51103216-buffy.jpg',
+  profile_url: 'https://upload.wikimedia.org/wikipedia/en/a/a2/S514_Buffy.png',
   cover_url: 'https://365thingsinhouston.com/wp-content/uploads/2015/10/myths-and-legends-of-the-graveyard-nmfh-696x407.jpg'
 )
 Instructor.create!(
@@ -33,7 +33,7 @@ Instructor.create!(
   user_id: user2.id,
   price: 89,
   bio: 'Witchcraft for all! I promise that in just a few short lessons you can also be a witch!',
-  profile_url: 'https://junkee.com/wp-content/uploads/2017/03/willow-2.jpg',
+  profile_url: 'https://openpsychometrics.org/tests/characters/test-resources/pics/BVS/3.jpg',
   cover_url: 'https://blogs.loc.gov/loc/files/2019/10/Screen-Shot-2019-10-29-at-6.45.46-PM-1024x608.png'
 )
 Instructor.create!(
@@ -43,7 +43,7 @@ Instructor.create!(
   user_id: user3.id,
   price: 5,
   bio: "There is abslutely nothing I am good at. I wouldn't book with me if I were you",
-  profile_url: 'https://m.media-amazon.com/images/M/MV5BMTM3NTA3MzM3N15BMl5BanBnXkFtZTYwMTY4NjEz._V1_UY1200_CR172,0,630,1200_AL_.jpg',
+  profile_url: 'https://openpsychometrics.org/tests/characters/test-resources/pics/BVS/2.jpg',
   cover_url: 'https://source.unsplash.com/HZrYYi6Orgg'
 )
 Instructor.create!(
@@ -53,7 +53,7 @@ Instructor.create!(
   user_id: user4.id,
   price: 55,
   bio: "I am broody and moody and can show you how to become one too...",
-  profile_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_u-knlE4hna_4fnfTkgMKPLLXpI6pm6dzlQ&usqp=CAU',
+  profile_url: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/top-1553084314.jpg?resize=480:*',
   cover_url: 'https://source.unsplash.com/hsg538WrP0Y'
 )
 Instructor.create!(
